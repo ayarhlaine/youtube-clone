e06d9142004177e6d750bfb77e3accdc06b8121d
Ayar Hlaine <ayarhlaine.dev@gmail.com>
2020-08-16 20:36:59 +0630

    Remove Unit Tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
 
 test('render App', () => {
-  render(<App />);
   expect(true).toBe(true);
 });
