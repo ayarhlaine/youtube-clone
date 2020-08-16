cf1651cb0c89d15d7c342571050a5e343d7341fd
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-16 17:00:18 +0000

    Deploy ayarhlaine/youtube-clone to github.com/ayarhlaine/youtube-clone.git:gh-pages

---
+++ precache-manifest.d482667057eaa9403bab6296415c881e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "226d1a179845da24fcbd1d163e96c4f0",
+    "url": "/youtube-clone/index.html"
+  },
+  {
+    "revision": "4068de4381e05abb512d",
+    "url": "/youtube-clone/static/css/main.c710f3c4.chunk.css"
+  },
+  {
+    "revision": "b9e9c3d872ae6b461bac",
+    "url": "/youtube-clone/static/js/2.d79921cb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/youtube-clone/static/js/2.d79921cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4068de4381e05abb512d",
+    "url": "/youtube-clone/static/js/main.b7f2450e.chunk.js"
+  },
+  {
+    "revision": "8a09cd451cb85b60110f",
+    "url": "/youtube-clone/static/js/runtime-main.a270c926.js"
+  }
+]);
